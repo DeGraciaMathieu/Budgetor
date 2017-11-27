7d57108195e01f5ae9a9b1eb4fc84ba97eb81508
DeGraciaMathieu <contact@degracia-mathieu.fr>
2017-11-26 22:46:01 -0800

    Création des pages vues

---
+++ resources/assets/js/pages/list-budgets.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <ol class="breadcrumb">
+              <li><a href="/">Home</a></li>
+              <li><a href="#">Budgets</a></li>
+              <li class="active">...</li>
+            </ol>
+            <panels-info-budget :budget="budget"></panels-info-budget>           
+            <panels-categories :categories="categories"></panels-categories>           
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+        }
+    },
+}
+</script>
--- resources/views/budget/show.blade.php
+++ resources/views/budget/show.blade.php
@@ -2,42 +2,11 @@
 
 @section('content')
 <div id='app'>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <ol class="breadcrumb">
-                  <li><a href="/">Home</a></li>
-                  <li><a href="#">Budgets</a></li>
-                  <li class="active">...</li>
-                </ol>
-                <panels-info-budget :budget="budget"></panels-info-budget>           
-                <panels-categories :categories="categories"></panels-categories>           
-            </div>
-        </div>
-    </div>
+    <pages-list-budgets></pages-list-budgets>
 </div>
 <script>
 new Vue({
-    el: '#app', 
-    data(){
-        return{
-            budget: [],
-            categories: [],
-        }
-    },    
-    created: function()
-    {
-        this.fetchData();
-    },
-    methods: {
-        fetchData: function()
-        {
-            axios.get(`/api/budget/12`).then(response => {
-                this.budget = response.data;
-            })
-            .catch(e => {});
-        },
-    }         
+    el: '#app',         
 });
 </script>
 @endsection
